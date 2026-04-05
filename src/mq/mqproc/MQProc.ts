2c04e34f9b655fbd89ae4a3c45bff0cf325ce718
DaviSM <davimesquita@gmail.com>
2026-04-05 00:01:21 -0300

    feat: inicio de rotas de processamento

--- src/mq/mqproc/MQProc.ts
+++ src/mq/mqproc/MQProc.ts
@@ -4,13 +4,29 @@ export default async function (rawmsg: Message<unknown>, env: Env) {
 	
 	let msg = rawmsg.body as MQCFGATEWAYMessage;
 	
-	// Out
-	await env.MQCFGATEWAY.send({
-		...msg,
-		type: 'out',
-		time: Date.now()
-	} as MQCFGATEWAYMessage, {
-		contentType: "json",
-	});
+	let path = new URL(msg.url).pathname;
+	
+	if (path === "/store") {
+		
+		// Store
+		await env.MQCFGATEWAY.send({
+			...msg,
+			type: 'out',
+		} as MQCFGATEWAYMessage, {
+			contentType: "json",
+		});
+		
+	} else {
+		
+		// Store - Lost
+		await env.MQCFGATEWAY.send({
+			...msg,
+			type: 'out',
+			time: Date.now(),
+		} as MQCFGATEWAYMessage, {
+			contentType: "json",
+		});
+		
+	}
 	
 }
